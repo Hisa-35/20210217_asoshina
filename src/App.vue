28b6de86c12f37beb8558e88fee3913670d0fd38
Hisa <hisasoccer@icloud.com>
2021-02-17 17:20:58 +0900

    d

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="container">
+    <input type="text"  v-model="text"/>
+    <button @click="searchAddressInfo">住所自動入力</button>
+      <p>Address：{{addressData['address']}}</p>
+  </section>
+</template>
+
+<script>
+const axios = require('axios');
+
+let url = 'postcodejp.address.AutoComplementService(IPJQQ8yzHDIjySikctA5hy3K2Yy5obbcpC4ra38)';
+
+export default {
+  data() {
+    return {
+      zipcode: '',
+      addressData: {}
+    }
+  },
+  methods: {
+    searchAddressInfo() {
+      axios.get(url + this.text).then((res) => {
+        this.addressData = res.data.results[0];
+      })
+    }
+  }
+}
+</script>
